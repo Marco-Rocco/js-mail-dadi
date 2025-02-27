cec95e69f47bbebd5daf8a0a1b6d5654e2f05470
Marco-Rocco <marcorocco28@icloud.com>
2025-02-27 16:34:39 +0100

    feat: create array

---
+++ mail/script.js
@@ -0,0 +1,10 @@
+let mail = [
+    "mongi@mail.com",
+    "marco@mail.com",
+    "luca@mail.com",
+    "pasquale@mail.com",
+    "danilo@mail.com",
+    "alessandra@mail.com",
+]
+
+console.log(mail)
