4377cb7dadf7d2b28c60d499692b8648deeaf077
Marco-Rocco <marcorocco28@icloud.com>
2025-02-27 18:01:08 +0100

    feat: BONUS allow the user only to use numbers and numbers between 1 and 6

--- dadi/script.js
+++ dadi/script.js
@@ -4,6 +4,11 @@ console.log('scelta computer: ' + computerGuess);
 
 //allow the user to choose his own number
 let userGuess = parseInt (prompt ('inserisci un numero tra 1 e 6'));
+
+//Bonus x2! set a while condition to repeat the funcion until the user inserts a valid value
+while (isNaN(userGuess) || userGuess < 1 || userGuess > 6) {
+    userGuess = parseInt(prompt("Numero non valido. Inserisci un numero tra 1 e 6:"));
+}
 console.log('scelta user: ' + userGuess);
 
 //alert message of result
