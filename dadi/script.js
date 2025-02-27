cf5210d951959cf87dd19d3eb2b52d90b77bef59
Marco-Rocco <marcorocco28@icloud.com>
2025-02-27 17:51:53 +0100

    feat: create if condition to declare a winner

--- dadi/script.js
+++ dadi/script.js
@@ -1,3 +1,16 @@
 //generate a random number between 1 and 6
 let computerGuess = Math.floor (Math.random() * 6) + 1;
-console.log(computerGuess)
+console.log('scelta computer: ' + computerGuess);
+
+//allow the user to choose his own number
+let userGuess = parseInt (prompt ('inserisci un numero tra 1 e 6'));
+console.log('scelta user: ' + userGuess);
+
+//alert message of result
+if (userGuess > computerGuess) {
+    alert ('Hai vinto!')
+} else if (userGuess === computerGuess){
+    alert ('Pareggio')
+} else {
+    alert ('Hai perso!')
+}
