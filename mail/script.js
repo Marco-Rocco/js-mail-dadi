b79f06b1c91621e21f0e48feeea6e5593bec390e
Marco-Rocco <marcorocco28@icloud.com>
2025-02-27 17:02:07 +0100

    feat: create a for cicle to verify the existance in the array

--- mail/script.js
+++ mail/script.js
@@ -6,12 +6,28 @@ let email = [
     "pasquale@mail.com",
     "danilo@mail.com",
     "alessandra@mail.com",
-]
+];
 
-console.log(email)
+console.log(email);
 //crate a prompt for user to insert an email address
+let itemToFind = prompt ("inserisci un indirizzo email");
 
-let itemToFind = prompt ("inserisci un indirizzo email")
+console.log("indirizzo email inserito dall'utente:" + ' ' + itemToFind);
 
-console.log("indirizzo email inserito dall'utente:" + ' ' + itemToFind)
+//dichiaro una variabile per tracciare lo status della ricerca
+let found = false;
 
+//create a for cicle to verify if the inserted email is present inside the array
+for (i = 0; i < email.length; i++) {
+    if (email[i] === itemToFind) {
+        found = true;
+        break;
+    }
+}
+
+//stampo l'esito
+if (found){
+    alert ("la mail è presente nell'array")
+} else {
+    alert ("la tua mail non è presente nell'array" )
+}
